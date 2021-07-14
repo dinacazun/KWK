51550f339b3a918b1bee9a9970639c85fecbfdb4
dinacazun <87436886+dinacazun@users.noreply.github.com>
2021-07-14 15:13:53 -0400

    Mini Project 1

--- KWK/StoryboardIntro/StoryboardIntro/ViewController.swift
+++ KWK/StoryboardIntro/StoryboardIntro/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("This is a print statement - testing, testing!")
+        //print("This is a print statement - testing, testing!")
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+        }
+    }
+    
 }
 
