bc1fff06abb19d03eef1530ac2bc02863d8d4d50
dinacazun <87436886+dinacazun@users.noreply.github.com>
2021-07-14 11:37:29 -0400

    1st Version

---
+++ KWK/StoryboardIntro/StoryboardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryboardIntro
+//
+//  Created by Dina Cazun on 7/14/21.
+//  Copyright © 2021 Dina Cazun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing, testing!")
+    }
+
+
+}
+
