a51dcddb82dfaf41cc681d6c2cd7863b0a05b0dd
dinacazun <87436886+dinacazun@users.noreply.github.com>
2021-07-15 11:43:58 -0400

    Segue Example

---
+++ KWK/SegueEx1/SegueEx1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueEx1
+//
+//  Created by Dina Cazun on 7/15/21.
+//  Copyright © 2021 Dina Cazun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
